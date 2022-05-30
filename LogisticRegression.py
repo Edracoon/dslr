b1272bdd926dbfa7df6d6b04641b0cf7ea0fd618
Edracoon <77042040+Edracoon@users.noreply.github.com>
2022-05-30 23:51:58 +0200

    closely done

---
+++ LogisticRegression.py
@@ -0,0 +1,111 @@
+"""
+
+Logistic Regression Object that can be used in futures ML projects !
+
+Prediction function = sigmoid( W.T * x + B)
+So for each W (feature), we have a probability (0 to 1) that a fact is true or false.
+In order to optimize our parameters (W and B) we have to perfom a gradient descent algorithm.
+For that we use an error function specifically called in this case : Binary Coss-Entropy Loss AKA Cost function.
+The partial derivatives of this Cost Function with respect to W and B will give use the direction
+in order to decrease the value of the Cost like so (just like in linear regression) :
+
+dw = (1 / n_samples) * X.T * (Ypred — y)
+db = (1 / n_samples) * (Ypred — y)
+
+n_samples = the number of samples (rows in training data)
+Ypred = the prediction function
+
+And so we can update our parameters :
+
+W = W - L * dW
+B = B - L * dB
+
+
+L is the learning rate -> Used in the Gradient Ascent Algorithm
+Epoch is the machine learning term for 'Iteration'
+W is the array of Weight of the length of the number of features in X
+B is the bias (the second parameter of the prediction formula)
+
+"""
+
+import numpy as np
+import math
+
+class LogisticRegression:
+
+    def __init__(self, L=0.001, epochs=1000):
+        self.L = L
+        self.epochs = epochs
+        self.W = None
+        self.B = 0
+
+    def train(self, X, y):
+        # Get the shape of X
+        n_samples, n_features = X.shape
+        
+        
+        # Init parameters weight and b to zeros
+        self.W = np.zeros((n_features, 1))
+        self.B = 0
+        
+        # Make the real values n_samples * 1
+        y = y.reshape(n_samples, 1)
+        
+        # Standardize our inputs avoiding scaling problem and optimum problem
+        X = self.standardize(X)
+                
+        for epoch in range(self.epochs):
+            # Compute prediction with our actual weights and bias
+            linear_prediction = np.dot(X, self.W) + self.B
+            
+            # Apply sigmoid function to our prediction, this will give us a probability (0 - 1)
+            prediction_proba = self.sigmoid(linear_prediction)
+            
+            # Compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (prediction_proba - y))
+            db = (1 / n_samples) * np.sum((prediction_proba - y))
+
+            # Update parameters
+            self.W -= self.L * dw
+            self.B -= self.L * db
+        
+        return self.W, self.B, prediction_proba
+
+    def classificate(self, predictions):
+        return [1 if predictions[i] > 0.5 else 0 for i in range(len(predictions))];
+
+    def standardize(self, X):
+        n_samples, n_features = X.shape
+        for i in range(n_features):
+            mean = self.mean(X.iloc[:,i])
+            std  = self.standard_deviation(X.iloc[:,i])
+            for j in range(len(X.iloc[:,i])):
+                if X.iloc[j,i] != X.iloc[j,i]:
+                    X.iloc[j,i] = 0
+                else:
+                    X.iloc[j,i] = (X.iloc[j,i] - mean) / std
+        return X
+
+    def sigmoid(self, x):
+        return (1.0 / (1.0 + np.exp(-x)))
+
+    def mean(self, values):
+        mean = 0
+        n = len(values)
+        for i in range(len(values)):
+            if values[i] != values[i]:
+                n -= 1
+            else:
+                mean += values[i]
+        return mean / n
+    
+    def standard_deviation(self, values):
+        mean = self.mean(values)
+        sum = 0
+        for row in range(len(values)):
+            if values[row] != values[row]:
+                sum += 0
+            else:
+                sum += (values[row] - mean) ** 2
+        return math.sqrt(sum / len(values))
+    
--- logreg_train.py
+++ logreg_train.py
@@ -1,16 +1,61 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
-#   It takes as a parameter dataset_test.csv and a file containing the weights trained by previous program.
-#   In order to evaluate the performance of your classifier this second program will have
-#   to generate a prediction file houses.csv formatted exactly as follows:
-#
-#   Index,Hogwarts House
-#   0,Gryffindor
-#   1,Hufflepuff
-#   2,Ravenclaw
-#   3,Hufflepuff
-#   4,Slytherin
-#   5,Ravenclaw
-#   6,Hufflepuff
+#   It takes as a parameter dataset_train.csv. You must use the
+#   technique of gradient descent to minimize the error. The program generates a file
+#   containing the weights that will be used for the prediction.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+import sys
+import pandas as pd
+import numpy as np
+from LogisticRegression import LogisticRegression
+
+fileName = ''
+if (len(sys.argv) < 2):
+    print('Error: Need a dataset as argument')
+    exit(1)
+else:
+    fileName = sys.argv[1]
+
+accuracy = False
+if (len(sys.argv) > 2 and sys.argv[2] == '-accuracy'):
+    accuracy = True
+
+def precision(reals, preds):
+    mean = 0
+    for i in range(len(reals)):
+        mean += abs(reals[i] - preds[i])
+    percent = 100 - (mean / len(reals))
+    return (round(percent, 4))
+
+# Load csv and fill our real values (1 or 0)
+X = pd.read_csv(fileName)
+Y1 = np.array([1 if X['Hogwarts House'][i] == 'Ravenclaw' else 0 for i in range(X.shape[0])])
+Y2 = np.array([1 if X['Hogwarts House'][i] == 'Slytherin' else 0 for i in range(X.shape[0])])
+Y3 = np.array([1 if X['Hogwarts House'][i] == 'Gryffindor' else 0 for i in range(X.shape[0])])
+Y4 = np.array([1 if X['Hogwarts House'][i] == 'Hufflepuff' else 0 for i in range(X.shape[0])])
+X = X.select_dtypes('float')
+
+
+file = open("weights.save", "w")
+
+# Initialize Logistic Regression class
+LogReg = LogisticRegression()
+W, B, pred1 = LogReg.train(X, Y1)
+file.write(f"{W} {B}")
+class1 = LogReg.classificate(pred1)
+W, B, pred2 = LogReg.train(X, Y2)
+file.write(f"{W} {B}")
+W, B, pred3 = LogReg.train(X, Y3)
+file.write(f"{W} {B}")
+W, B, pred4 = LogReg.train(X, Y4)
+file.write(f"{W} {B}")
+
+file.close()
+
+print(f"For Ravenclaw  : {precision(Y1, class1)}" + "%" + " of accuracy classification")
+print(f"For Slytherin  : {precision(Y2, pred2)}" + "%" + " of accuracy classification")
+print(f"For Gryffindor : {precision(Y3, pred3)}" + "%" + " of accuracy classification")
+print(f"For Hufflepuff : {precision(Y4, pred4)}" + "%" + " of accuracy classification")
+
